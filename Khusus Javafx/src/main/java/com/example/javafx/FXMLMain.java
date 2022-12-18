c57d30f821784ca89a04b2b3c7f7f844ef318b50
Muflihys <119804435+Muflihys@users.noreply.github.com>
2022-12-18 16:36:21 +0700

    Add files via upload

---
+++ Khusus Javafx/src/main/java/com/example/javafx/FXMLMain.java
@@ -0,0 +1,24 @@
+package com.example.javafx;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.time.YearMonth;
+
+public class FXMLMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Calender"));
+        primaryStage.setTitle("Calender");
+        primaryStage.setScene(new Scene(loader.load()));
+        // Get the controller and add the calendar view to it
+        Controller controller = loader.getController();
+        controller.calendarPane.getChildren().add(new FullCalendarView(YearMonth.now()).getView());
+        primaryStage.show();
+    }
+}
+
